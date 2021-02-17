55b700bdd41cb835b6d9089ed4ec58753ed2c0b9
Surya Jasper <45545431+suryajasper@users.noreply.github.com>
2021-02-16 22:24:12 -0800

    cleaned up server code and added documentation

--- website/server.js
+++ website/server.js
@@ -9,7 +9,6 @@ app.use((req, res, next) => {
   next();
 });
 app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(bodyParser.json());
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 2000;
@@ -18,12 +17,19 @@ const mongoose = require('mongoose');
 const User = require('./models/user');
 const Document = require('./models/document');
 const File = require('./models/file');
+const { deprecate } = require('util');
 
 const uri = "mongodb+srv://nanocheck:iPNNEole7FRCFplF@noteversecluster.oxlnj.mongodb.net/documents";
 mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
 var db = mongoose.connection;
 
-
+/**
+ * Sign up new user
+ * @param username
+ * @param email
+ * @param password
+ * @returns new user's UUID
+ */
 app.post('/createUser', (req, res) => {
   console.log('newuser', req.query);
   if (!(req.query && req.query.username && req.query.email && req.query.password)) return res.status(400).send('no data');
@@ -45,6 +51,12 @@ app.post('/createUser', (req, res) => {
   })
 });
 
+/**
+ * Log in existing user
+ * @param email
+ * @param password
+ * @returns UUID if valid
+ */
 app.post('/authenticateUser', (req, res) => {
   console.log('login', req.query);
   if (!(req.query && req.query.password && req.query.email)) return res.status(400).send('no data');
@@ -62,6 +74,12 @@ app.post('/authenticateUser', (req, res) => {
   })
 })
 
+/**
+ * Creates new note document for the given user
+ * @param authorUUID: author's UUID
+ * @param fileName
+ * @param parentFolderId: _id of folder containing the new file
+ */
 app.post('/newDocument', (req, res) => {
   var docInfo = req.query;
   console.log('new document params', req.query);
@@ -93,6 +111,12 @@ app.post('/newDocument', (req, res) => {
   res.status(201).send('added document');
 })
 
+/**
+ * Creates new folder for the given user
+ * @param uid: author's UUID
+ * @param fileName: name of new folder
+ * @param parentFolderId: _id of folder containing the new folder
+ */
 app.post('/newFolder', (req, res) => {
   var folderInfo = req.query;
   console.log(req.query);
@@ -111,6 +135,12 @@ app.post('/newFolder', (req, res) => {
   res.status(201).send('added document');
 })
 
+/**
+ * Update file/folder based on query
+ * @param query: query object to find file/folder
+ * @param update: object with key-value pairs to update
+ * @returns old name and new name of file/folder
+ */
 app.post('/updateFolder', (req, res) => {
   var update = req.query;
   console.log('update', update);
@@ -125,6 +155,10 @@ app.post('/updateFolder', (req, res) => {
   return res.json({oldName: update.query.fileName, newName: update.update.fileName});
 })
 
+/**
+ * Delete file or folder
+ * @param idToDelete: _id of file/folder to remove
+ */
 app.post('/deleteFile', (req, res) => {
   var query = req.query;
 
@@ -139,6 +173,12 @@ app.post('/deleteFile', (req, res) => {
   });
 })
 
+/**
+ * Get files/folders in given folder
+ * @param uid: authorUUID
+ * @param parentFolderId: _id of parent folder (id=root if in root folder)
+ * @returns array of files/folders
+ */
 app.get('/getDocuments', async (req, res) => {
   var pathInfo = req.query;
   console.log(req.query);
@@ -173,25 +213,6 @@ app.get('/getDocuments', async (req, res) => {
   return res.status(401).send('could not find location');
 })
 
-app.post('/addCollaborator', async (req, res) => {
-  var collabInfo = req.query;
-  if (!collabInfo) {
-    return res.status(400).send('missing query');
-  }
-
-  var userInfo = await User.findOne({email: req.query.email});
-  if (userInfo == null) return res.status(400).send('no user with matching credentials');
-
-  var sharedFile = new File({
-    isPointer: true,
-    pointerTo: collabInfo.fileId
-  })
-
-  sharedFile.save();
-
-  return res.status(200);
-})
-
 app.get('/document', (req, res) => {
   if (req.query && req.query.name) {    
     db.collection('documents').findOne({name: req.query.name}, (err, document) => {
@@ -203,6 +224,11 @@ app.get('/document', (req, res) => {
   }
 })
 
+/**
+ * get user info based on query object
+ * @param query
+ * @returns user info
+ */
 app.get('/getUser', (req, res) => {
   User.findOne(req.query.query, (err, document) => {
     if (err) return res.status(404).send('no such user found');
@@ -212,6 +238,11 @@ app.get('/getUser', (req, res) => {
   });
 })
 
+/**
+ * get list of users that user has previously shared docs with
+ * @param uid: authorUUID
+ * @returns array of user infos for each previous collaborator
+ */
 app.get('/getPreviousCollaborators', async (req, res) => {
   console.log('bruh', req.query);
   var userInfo = await User.findById(req.query.uid).exec();
@@ -228,6 +259,11 @@ app.get('/getPreviousCollaborators', async (req, res) => {
   return res.json(collaborators);
 })
 
+/**
+ * get collaborators for file/folder
+ * @param fileId: _id of fild/folder
+ * @returns array of each collaborator's access permissions
+ */
 app.get('/getCollaborators', async (req, res) => {
   console.log('hi');
   var fileInfo = await File.findById(req.query.fileId);
@@ -243,13 +279,25 @@ app.get('/getCollaborators', async (req, res) => {
   return res.json([]);
 })
 
+/**
+ * Add/remove/update collaborators for file/folder
+ * Receives user permissions
+ *    Adds new collaborators
+ *    Updates existing collaborators
+ *    Deletes collaborators that are not received
+ * @param uid: authorUUID
+ * @param fileId: _id for file/folder
+ * @param collaborators: array of user permission objects
+ *    @param collaborator.authorUID
+ *    @param collaborator.editingMode: either viewing or editing
+ *    @param collaborator.canShare: whether the collaborator can share the file/folder with others
+ */
 app.post('/updateCollaborators', async (req, res) => {
   var userInfo = await User.findById(req.query.uid).exec();
   if (!userInfo) return res.status(400).send('wrong uid');
 
   var fileInfo = await File.findById(req.query.fileId).exec();
   if (!fileInfo) return res.status(400).send('no file');
-
   
   var currCollabIds = [];
   for (var userPerm of fileInfo.userPermissions) {
@@ -262,11 +310,6 @@ app.post('/updateCollaborators', async (req, res) => {
 
   for (var i = 0; i < req.query.collaborators.length; i++) {
     let collaborator = req.query.collaborators[i];
-    /*var perms = {
-      authorUID: collaborator.authorUID,
-      editingMode: collaborator.editingMode,
-      canShare: collaborator.canShare
-    };*/
     resUIDs.push(collaborator.authorUID);
 
     console.log(`[${i}/${req.query.collaborators.length}] started update`);
